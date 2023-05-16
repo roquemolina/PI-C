1b3d0e8b46fd63e0a0b7e0f83db1489f94001d1c
Roque Molina <roque.nicolas.molina@gmail.com>
2023-05-16 15:44:43 -0300

    back listo con todo

--- PI-Countries-main/api/src/handlers/activitiesHandlers.js
+++ PI-Countries-main/api/src/handlers/activitiesHandlers.js
@@ -12,6 +12,7 @@ const getActivityHandler = async (req, res) => {
 const postActivityHandler = async (req, res) => {
   const {name, difficulty, length, season, countries } = req.body;
     try {
+      if(countries.length === 0) throw new Error ('pal orto');
       const newActivity = await createActivity(name, difficulty, length, season, countries);
       res.status(200).json(newActivity);
     } catch (error) {
