05f89198ac4a17d9d1ec8648299cf9746200ca65
Roque Molina <roque.nicolas.molina@gmail.com>
2023-06-02 20:04:34 -0300

    DEPLOYED

--- PI-Countries-main/client/src/index.js
+++ PI-Countries-main/client/src/index.js
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3001/"; 
-/* axios.defaults.baseURL = "https://pi-back-production-5037.up.railway.app/"; */
+/*axios.defaults.baseURL = "http://localhost:3001/"; */
+axios.defaults.baseURL = "https://pi-back-production-5037.up.railway.app/";
 
 
 
